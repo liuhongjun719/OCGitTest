772bbb6529e5fee5c4a0ecb736bff3ed05568a81
liuhongjun <357213063@qq.com>
2016-12-26 13:07:35 +0800

    Test git demo one.
    Test git demo one.

---
+++ OCGitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCGitTest
+//
+//  Created by 123456 on 16/12/26.
+//  Copyright © 2016年 123456. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OCGitTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  OCGitTest
+//
+//  Created by 123456 on 16/12/26.
+//  Copyright © 2016年 123456. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
